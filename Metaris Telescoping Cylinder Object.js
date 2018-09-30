a956a9b206fa076b16ba3211e59aa2a48e39b59d
Alex Kaprosy <afkaprosy@gmail.com>
2018-09-30 08:27:02 -0700

    Add files via upload

---
+++ Metaris Telescoping Cylinder Object.js
@@ -0,0 +1,54 @@
+/*Metaris telescoping cylinder object project*/
+class Telecyl {
+    constructor(part_number, stroke, closed, extended, base_eye_width,base_pin_dia, rod_eye_width, rod_eye_dia){
+        this.part_number = part_number
+        this.stroke = stroke
+        this.closed = closed
+        this.extended = extended
+        this.base_eye_width = base_eye_width
+        this.base_pin_dia = base_pin_dia
+        this.rod_eye_width = rod_eye_width
+        this.rod_eye_dia = rod_eye_dia
+	}
+}
+var telecyl1 = new Telecyl("MH100-53-84P", 84,39.5,122.5,7,2,2,2);
+
+var telecyl2 = new Telecyl("MH100-53-126P", 126,54.5,181.25,7,2,2,2);
+
+var telecyl3 = new Telecyl("MH100-63-108P", 108,49.5,157.5,7.25,2,2,2);
+
+var telecyl4 = new Telecyl("MH100-63-120P", 120,53.5,173.5,7.25,2,2,2);
+
+var telecyl5 = new Telecyl("MH100-63-126P", 126,55.5,181.5,7.25,2,2,2);
+
+var telecyl6 = new Telecyl("MH100-63-132P", 132,57.375,189.375,7.25,2,2,2);
+
+var telecyl7 = new Telecyl("MH100-63-140P", 140, 60, 200, 7.25, 2,2,2);
+
+var telecyl8 = new Telecyl("MH100-64-135P-175", 135,48.25,183,8,1.75,2.75,1.5);
+
+var telecyl9 = new Telecyl("MH100-64-156P-175", 156,53.5,209.5,7.25,1.75,2.75,1.5);
+
+var telecyl10 = new Telecyl("MH100-64-156P", 156,53.625, 209.625,7.25,2,1.5,2);
+
+var telecyl11 = new Telecyl("MH100-74-135P", 135,48.5,183.5,8.625,2,2,2);
+
+var telecyl12 = new Telecyl("MH100-74-156P", 156,53.75,209.75,8.625,2,2,2);
+
+var telecyl13 = new Telecyl("MH100-85-190P", 190,54,233.25,9.5,2,2,2);
+
+var telecyl14 = new Telecyl("MH100-85-220P", 220,60,269.25,9.5,2,2,2);
+
+var telecyl15 = new Telecyl("MH100-85-235P", 235,65,299.25,9.5,2,2,2);
+
+var telecyl16 = new Telecyl("MH100-85-250P", 250,68,317.25,9.5,2,2,2);
+
+var telecyl17 = new Telecyl("MH100-85-265P", 265,74,335.25,9.5,2,2,2);
+
+
+
+
+
+
+
+
